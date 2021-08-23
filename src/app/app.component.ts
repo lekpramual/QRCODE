6b478e01441e7d0ef9878369f17dd2b80e612d5e
lekpramual <lek.pramual@gmail.com>
2021-08-23 16:32:26 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
 
   title = 'ng-line-liff';
   messages: string;
-  userProfile!: any ;
+  userProfile!: any | undefined;
 
   // profile: UnPromise<ReturnType<typeof liff.getProfile>> | undefined;
 
